bb421f5d4428a23486d6b892170a90afa81a24bf
Dustin Etts <dustinetts@gmail.com>
2026-02-22 16:13:51 -0500

    Centralizing and fixing styling, adding light/dark modes

--- src/app/components/header-bar/header-bar.component.ts
+++ src/app/components/header-bar/header-bar.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { AuthState } from '../../state/reducers/auth.reducer';
 import { selectAuthState } from '../../state/selectors/auth.selectors';
 import { OidcService } from '../../services/auth/oidc.service';
+import { ThemeService } from '../../services/theme.service';
 
 @Component({
   selector: 'app-header-bar',
@@ -14,14 +15,31 @@ import { OidcService } from '../../services/auth/oidc.service';
   templateUrl: './header-bar.component.html',
   styleUrl: './header-bar.component.scss'
 })
-export class HeaderBarComponent {
+export class HeaderBarComponent implements OnInit, OnDestroy {
   authState$: Observable<AuthState>;
+  isDark = false;
+  private themeSub?: Subscription;
 
   constructor(
     private store: Store,
-    private oidcService: OidcService
+    private oidcService: OidcService,
+    private themeService: ThemeService
   ) {
     this.authState$ = this.store.select(selectAuthState);
+  }
+
+  ngOnInit(): void {
+    this.themeSub = this.themeService.currentTheme.subscribe(
+      theme => this.isDark = theme === 'dark'
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.themeSub?.unsubscribe();
+  }
+
+  toggleTheme(): void {
+    this.themeService.toggleTheme();
   }
 
   async login(): Promise<void> {
